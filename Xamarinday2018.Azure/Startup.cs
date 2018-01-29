0f3ecc2b4fc1e911511d682c6acda62516cf22c4
Denis Kretov <kretov.d@gmail.com>
2018-01-29 11:11:16 +0300

    Настроил таймер для SignalR

--- Xamarinday2018.Azure/Startup.cs
+++ Xamarinday2018.Azure/Startup.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
 
 namespace Xamarinday2018.Azure
 {
     public class Startup
     {
+        private static string _color = "fff";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -15,13 +21,24 @@ namespace Xamarinday2018.Azure
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider, ILoggerFactory loggerfactory, IApplicationLifetime applicationLifetime)
         {
             app.UseStaticFiles();
             app.UseSignalR(routes =>
             {
-                routes.MapHub<XamarinDay>("hubs");
+                routes.MapHub<XamarinDayHub>("hubs");
             });
+
+
+            void TimerCallback(object x)
+            {
+                var hub = serviceProvider.GetService<IHubContext<XamarinDayHub>>();
+                hub.Clients.All.InvokeAsync("Color", _color);
+                _color = _color == "fff" ? "000" : "fff";
+            }
+
+            var timer = new Timer(TimerCallback);
+            timer.Change(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1));
 
             if (env.IsDevelopment())
             {
---
+++ Xamarinday2018.Azure/XamarinDayHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Xamarinday2018.Azure
+{
+    public class XamarinDayHub : Hub
+    {
+
+    }
+}

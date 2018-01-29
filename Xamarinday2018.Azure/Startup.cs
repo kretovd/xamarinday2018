e25abc76b89cee69bb745c9945e05bc84a7c4355
Denis Kretov <kretov.d@gmail.com>
2018-01-29 10:23:18 +0300

    SignalR added

--- Xamarinday2018.Azure/Startup.cs
+++ Xamarinday2018.Azure/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,11 +11,18 @@ namespace Xamarinday2018.Azure
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<XamarinDay>("hubs");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ Xamarinday2018.Azure/XamarinDay.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Xamarinday2018.Azure
+{
+    public class XamarinDay : Hub
+    {
+        public async Task Send(string color)
+        {
+            await Clients.All.InvokeAsync("Get", color);
+        }
+    }
+}
